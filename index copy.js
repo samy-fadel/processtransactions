c04220f7855b2add7af9c53e33b4d5ed2b1cdc7c
samy fadel <samyfadel@google.com>
2023-07-19 17:48:10 +0200

    samy

--- index copy.js
+++ index copy.js
@@ -10,6 +10,8 @@ const smartContractsTopicName = 'smart-contracts-transactions';
 const client = new v1.SubscriberClient();
 const pubsub = new PubSub({ projectId });
 
+console.log("line 13 before all code ");
+
 // Function to retrieve the API key from Secret Manager
 async function getApiKey() {
   const secretName = `projects/${process.env.PROJECT_NUMBER}/secrets/web3-api-key/versions/latest`;
@@ -18,8 +20,9 @@ async function getApiKey() {
   return version.payload.data.toString();
 }
 
-async function publishSmartContractAddress(address) {
-  const data = Buffer.from(JSON.stringify({ address }));
+async function publishTransaction(transaction) {
+  console.log("line 24 publishTransaction ");
+  const data = Buffer.from(JSON.stringify(transaction));
   await pubsub.topic(smartContractsTopicName).publish(data);
 }
 
@@ -33,7 +36,10 @@ async function handleError(message) {
   }
 }
 
+
+
 async function retrieveTransactions() {
+  console.log("line 40 retrieveTransactions ");
   const apiKey = await getApiKey();
   const web3 = new Web3(`https://mainnet.infura.io/v3/${apiKey}`);
 
@@ -42,23 +48,22 @@ async function retrieveTransactions() {
       subscription: client.subscriptionPath(process.env.PROJECT_ID, subscriptionName),
       maxMessages: 10, // Adjust the maxMessages value as needed
     };
-
+    console.log("line 49 try retrieveTransactions ");
     const [response] = await client.pull(request);
     const messages = response.receivedMessages;
+    // console.log(messages);
 
     if (messages && messages.length > 0) {
       for (const message of messages) {
         const transaction = JSON.parse(message.message.data.toString());
         console.log("line 65 ", transaction);
 
-        // Get the transaction receipt to check if it's a contract creation
-        //        const receipt = await web3.eth.getTransactionReceipt(transaction.hash);
-        const receipt = await web3.eth.getTransactionReceipt(transaction);
-        const isSmartContractCreation = receipt.contractAddress !== null;
+        // Check if the transaction is a smart contract creation transaction
+        const isSmartContractCreation = !transaction.to || transaction.to.trim() === '';
 
         if (isSmartContractCreation) {
-          // If it's a smart contract creation, publish the contract address as an individual message
-          await publishSmartContractAddress(receipt.contractAddress);
+          // If it's a smart contract creation, publish it to the topic
+          await publishTransaction(transaction);
         } else {
           console.log("Not a smart contract creation transaction.");
         }
@@ -88,5 +93,7 @@ async function retrieveTransactions() {
     }
   }
 }
+
+
 
 retrieveTransactions();
