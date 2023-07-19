4a29fb869b7405049ba9c18a7f8982f870a81910
samy fadel <samyfadel@google.com>
2023-07-19 12:28:24 +0200

    samy

--- index.js
+++ index.js
@@ -1,41 +1,3 @@
-const { PubSub } = require('@google-cloud/pubsub');
-const Web3 = require('web3');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
-const { v1 } = require('@google-cloud/pubsub');
-
-const projectId = process.env.PROJECT_ID;
-const subscriptionName = 'transactions-topic-sub';
-const smartContractsTopicName = 'smart-contracts-transactions';
-
-const client = new v1.SubscriberClient();
-const pubsub = new PubSub({ projectId });
-
-console.log("line 13 before all code ");
-
-// Function to retrieve the API key from Secret Manager
-async function getApiKey() {
-  const secretName = `projects/${process.env.PROJECT_NUMBER}/secrets/web3-api-key/versions/latest`;
-  const client = new SecretManagerServiceClient();
-  const [version] = await client.accessSecretVersion({ name: secretName });
-  return version.payload.data.toString();
-}
-
-async function publishTransaction(transaction) {
-  console.log("line 24 publishTransaction ");
-  const data = Buffer.from(JSON.stringify(transaction));
-  await pubsub.topic(smartContractsTopicName).publish(data);
-}
-
-async function handleError(message) {
-  try {
-    const data = Buffer.from(JSON.stringify(message));
-    await pubsub.topic(subscriptionName).publish(data);
-    console.log('Message put back in Pub/Sub subscription:', message);
-  } catch (error) {
-    console.error('Error handling error:', error);
-  }
-}
-
 async function retrieveTransactions() {
   console.log("line 40 retrieveTransactions ");
   const apiKey = await getApiKey();
@@ -49,31 +11,21 @@ async function retrieveTransactions() {
     console.log("line 49 try retrieveTransactions ");
     const [response] = await client.pull(request);
     const messages = response.receivedMessages;
-   // console.log(messages);
+    // console.log(messages);
 
     if (messages && messages.length > 0) {
-      //console.log("line 55 inside if");
-      //const transactionMessages = messages.map((msg) => JSON.parse(msg.message.data.toString())); 
-     // const transactionMessages = JSON.parse(message.data.toString());
-     // console.log("line 58 tx msg", transactionMessages);
-     // const transactions = transactionMessages.map((transactionMsg) => transactionMsg.transaction);
-     // console.log("line 59", transactions);
-
-
       const message = messages[0].message;
-      const messageData = message.data.toString();
-      console.log("line 65 ", messageData);
+      const transaction = JSON.parse(message.data.toString());
+      console.log("line 65 ", transaction);
 
+      // Check if the transaction is a smart contract creation transaction
+      const isSmartContractCreation = !transaction.to || transaction.to.trim() === '';
 
-      const sortedTransactions = transactions.filter((transaction) => {
-        console.log("line 60 inside sortedtx")
-        // Check if the transaction is a smart contract creation transaction
-        return transaction && (!transaction.to || transaction.to.trim() === '');
-      });
-
-      for (const transaction of sortedTransactions) {
-        console.log("line 59 ", sortedTransactions);
+      if (isSmartContractCreation) {
+        // If it's a smart contract creation, publish it to the topic
         await publishTransaction(transaction);
+      } else {
+        console.log("Not a smart contract creation transaction.");
       }
 
       const ackRequest = {
@@ -100,5 +52,3 @@ async function retrieveTransactions() {
     }
   }
 }
-
-retrieveTransactions();
